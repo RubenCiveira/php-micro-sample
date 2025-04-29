16153ed1506afd680454c99831f9c329bfb84ae4
Ruben civeira <rubenciveira@gmail.com>
2025-04-29 11:59:52 +0200

    format: docs

--- packages/civi/micro/src/Kernel/ObjectMapper.php
+++ packages/civi/micro/src/Kernel/ObjectMapper.php
@@ -4,8 +4,26 @@ declare(strict_types=1);
 
 namespace Civi\Micro\Kernel;
 
+/**
+ * ObjectMapper
+ *
+ * Utility class that provides methods to map between arrays and objects.
+ * It supports transforming an object into an array and constructing an object from an associative array,
+ * including type casting based on the target property types.
+ */
 class ObjectMapper
 {
+    /**
+     * Converts an object or an array into an array.
+     *
+     * If the input is already an array, it is returned as-is.
+     * If it is an object, its public properties are extracted into an associative array,
+     * omitting any properties with null values.
+     *
+     * @param array|object $object The array or object to convert.
+     *
+     * @return array The resulting array representation.
+     */
     public function toArray(array|object $object): array
     {
         if (is_array($object)) {
@@ -22,6 +40,23 @@ class ObjectMapper
         }
     }
 
+    /**
+     * Creates an object of the given type from an associative array.
+     *
+     * Properties are assigned based on their names. 
+     * If a property has a declared type, the method attempts to cast the value accordingly:
+     * - `int`, `float`, `bool`, and `string` are cast natively.
+     * - `DateTimeInterface` properties are instantiated from a string value.
+     * - If the property is nullable and the value is `null`, it is assigned as `null`.
+     * - If no type is declared or the type is not specifically handled, the value is assigned directly.
+     *
+     * Properties that do not exist in the target class are ignored.
+     *
+     * @param array|null $data      The associative array of data to map.
+     * @param string     $typeName  The fully qualified class name to instantiate.
+     *
+     * @return object|null The constructed object, or null if input data is null.
+     */
     public function toObject(array|null $data, string $typeName): object|null
     {
         if ($data === null) {
