a3c1a4fbe9c42b79fd3a54c8a09d3fa028c2f24b
Ruben civeira <rubenciveira@gmail.com>
2025-04-30 09:41:11 +0200

    feat: acces guard with logger

--- packages/civi/security/src/Guard/AccessGuard.php
+++ packages/civi/security/src/Guard/AccessGuard.php
@@ -1,13 +1,21 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Civi\Security\Guard;
 
 use Civi\Micro\Kernel\AbstractPipeline;
+use Civi\Micro\Telemetry\LoggerAwareInterface;
+use Civi\Micro\Telemetry\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
 
-class AccessGuard extends AbstractPipeline
+class AccessGuard extends AbstractPipeline implements LoggerAwareInterface
 {
+    use LoggerAwareTrait;
+
     public function canExecute(string $action, string $namespace, string $typeName, array $context, array $original): bool
     {
+        $this->logWarning("Trying to check canExecute {$action} on {$namespace} for {$typeName}");
         return true;
     }
 }
