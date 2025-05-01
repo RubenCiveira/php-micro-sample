80b3fd8d801cdc8cbdf7b64c0bd6a6d3741cc706
Ruben civeira <rubenciveira@gmail.com>
2025-05-01 11:28:16 +0200

    refactor: package naming

--- packages/civi/security-simple-store/bootstrap/register.php
+++ packages/civi/security-simple-store/bootstrap/register.php
@@ -5,6 +5,6 @@ use Civi\Security\Policy\PolicyEngine;
 use Civi\Store\Schemas;
 
 Schemas::register('usermanagement', __DIR__.'/../config/schemas/usermanagement');
-PolicyEngine::register( __DIR__.'/../conf/guards.yaml');
+PolicyEngine::register( __DIR__.'/../config/guards.yaml');
 AppBuilder::dependencies(__DIR__.'/di.container.php');
 AppBuilder::routes(__DIR__.'/slim.routes.php');
